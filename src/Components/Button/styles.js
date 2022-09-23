e8586f64fb72d4a1e29d75b47e3e7c958781cc2f
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-09-23 15:28:47 -0300

    Style: Criei um novo button sem Background

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -1,16 +1,32 @@
 import styled from "styled-components";
 
 export const ButtonGlobal = styled.button`
-  border: 1px solid var(--color-main);
+  border: none;
   border-radius: 5px;
-  background-color: var(--color-secondary);
+  background-color: black;
   color: white;
-  padding: 10px 15px;
+  padding: 10px 20px;
+  font-weight: 500;
   font-size: 16px;
   cursor: pointer;
   transition: all 0.2s;
 
   &:hover{
-    background-color: #1153cF;
+    background-color: var(--color-secondary);
+  }
+`
+
+export const ButtonGlobalNoBG = styled.button`
+  border: 1px solid black;
+  background-color: white;
+  color: var(--color-secondary);
+  padding: 10px 20px;
+  font-size: 15px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover{
+    background-color: #f3f4f6;
   }
 `
