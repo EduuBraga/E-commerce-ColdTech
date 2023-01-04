4ce83116f1614ecc683b41f30ff109e1369219ef
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-04 18:43:21 -0300

    Chore: Removi o backend da aplicação

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-const startDB = require('./configs/mongodb.js');
-const PORT = process.env.PORT || 3000;
-
-startDB();
-
-app.listen(PORT, () => console.log('Server is running...'));
